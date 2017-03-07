d4f37476d331c5c8dc740cd7d4dd8ad8e896e554
Marc Bittner <marc.bittner@grindr.com>
2017-03-07 14:05:30 -0800

    Solved ReverseBubbleSort

--- 001_bubble_sort/practice_problems/001_reverse_sort_with_numbers/main.go
+++ 001_bubble_sort/practice_problems/001_reverse_sort_with_numbers/main.go
@@ -2,14 +2,39 @@ package main
 
 import "fmt"
 
-func main() {
-	var numbers []int = []int{21, 4, 2, 13, 10, 0, 19, 11, 7, 5, 23, 18, 9, 14, 6, 8, 1, 20, 17, 3, 16, 22, 24, 15, 12}
-	fmt.Println("Unsorted:", numbers)
-	reverseBubbleSort(numbers)
-	fmt.Println("Sorted:  ", numbers)
+func reverseSweep(numbers []int) {
+
+		var firstIndex int = len(numbers) - 1
+		var secondIndex int = len(numbers) - 2
+
+		for secondIndex >= 0 {
+			var firstNumber int = numbers[firstIndex]
+      var secondNumber int = numbers[secondIndex]
+
+      if firstNumber > secondNumber {
+        numbers[firstIndex] = secondNumber
+        numbers[secondIndex] = firstNumber
+      }
+
+			firstIndex--
+			secondIndex--
+		}
+
 }
 
-// This should sort numbers IN REVERSE ORDER!!!
 func reverseBubbleSort(numbers []int) {
-	// Implement this and any other functions you may need
+  var N int = len(numbers)
+  var i int
+  for i = 0; i < N; i++ {
+    reverseSweep(numbers)
+  }
+}
+
+func main() {
+
+	var numbers []int = []int{21, 4, 2, 13, 10, 0, 19, 11, 7, 5, 23, 18, 9, 14, 6, 8, 1, 20, 17, 3, 16, 22, 24, 15, 12}
+  fmt.Println("Unsorted:", numbers)
+  reverseBubbleSort(numbers)
+  fmt.Println("Sorted:  ", numbers)
+
 }
