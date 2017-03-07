1b369e0bd67b3be765b3505e4507e1c8cce648a5
Marc Bittner <marc.bittner@grindr.com>
2017-03-07 15:30:10 -0800

    Added solution for string bubble sort

--- 001_bubble_sort/practice_problems/002_sort_strings/main.go
+++ 001_bubble_sort/practice_problems/002_sort_strings/main.go
@@ -1,6 +1,52 @@
 package main
 
 import "fmt"
+import "strings"
+
+func greater(a, b string) bool {
+	if strings.ToLower(a) > strings.ToLower(b) {
+		return true
+	} else {
+		return false
+	}
+}
+
+
+func sweep(animals []string) {
+
+//	var numberOfElements = len(animals)
+		var firstIndex int = 0
+		var secondIndex int = 1
+
+// 		fmt.Println("numberOfElements:", len(animals))
+//		fmt.Println("firstIndex:", firstIndex)
+//		fmt.Println("secondIndex:", secondIndex)
+// 		fmt.Println("animals value #1:", animals[firstIndex])
+// 		fmt.Println("animals value #2:", animals[secondIndex])
+
+		 for secondIndex + 1 <= len(animals) {
+
+		 	var firstString string = animals[firstIndex]
+      var secondString string = animals[secondIndex]
+
+       if strings.ToLower(firstString) > strings.ToLower(secondString) {
+         animals[firstIndex] = secondString
+         animals[secondIndex] = firstString
+       }
+
+		firstIndex++
+		secondIndex++
+		}
+
+}
+
+func bubbleSort(animals []string) {
+   var N int = len(animals)
+   var i int
+   for i = 0; i < N; i++ {
+     sweep(animals)
+   }
+ }
 
 func main() {
 	var animals []string = []string{
@@ -26,10 +72,7 @@ func main() {
 	}
 
 	fmt.Println("Unsorted:", animals)
+	//sweep(animals)
 	bubbleSort(animals)
 	fmt.Println("Sorted:  ", animals)
-}
-
-func bubbleSort(animals []string) {
-	// Implement this and any other functions you may need
 }
